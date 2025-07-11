95197e0dfe37adb0234560e97f913df73ec96d83
Metal Whale <metalwhale.dev@gmail.com>
2025-07-11 12:27:03 +0000

    Train a minimal DDPM.

--- minimal_ddpm/main.py
+++ minimal_ddpm/main.py
@@ -1,5 +1,17 @@
+import pathlib
+
+from minimal_ddpm.data import MultimodalDistribution
+from minimal_ddpm.model import MinimalDdpm, train
+
+
+STORAGE_PATH = pathlib.Path(__file__).parent.parent / "storage"
+
+
 def main():
-    print("Hello from minimal-ddpm!")
+    target_distribution = MultimodalDistribution([(-2.0, 1.0, 0.5), (1.0, 0.5, 0.5)])
+    # target_distribution = SillyDistribution((-10, 10), 20, (1, 1000))
+    model = MinimalDdpm()
+    train(STORAGE_PATH, target_distribution, model)
 
 
 if __name__ == "__main__":
